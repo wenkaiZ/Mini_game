5737f0c17ea056a24a0c6f55ae123fabb837336a
Wenkai Zheng <zheng.wenk@husky.neu.edu>
2019-03-23 19:21:22 -0400

    Create Repo

---
+++ Mini_game/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mini_game.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
